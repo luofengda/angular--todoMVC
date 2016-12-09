2c40f1e7327735ee23a56d5b72c34c56ab4854f8
luofengda <362861693@qq.com>
2016-12-09 15:37:03 +0800

    给列表动态添加数据[C

--- js/app.js
+++ js/app.js
@@ -11,10 +11,30 @@
 			{id:1,name:'张三',isCompleted:false},
 			{id:2,name:'李四',isCompleted:true},
 			{id:3,name:'html',isCompleted:false},
-			{id:4,name:'angular',isCompleted:true},
-			{id:5,name:'vue',isCompleted:false}
+			{id:4,name:'angular',isCompleted:true}
+		
 		];
-
+		// 任务二：
+		// 思路：
+		// 		获取数据添加到todolist
+		$scope.newTask='';
+		$scope.add=function(){
+			var id=0;
+			// 将数据添加
+			if (!$scope.newTask) {
+				return;
+			};
+			// id解决方法
+			// 取出数据中最后一条ID，然后加+1
+			if ($scope.todolist.length===0) {
+				id=1;
+			}else {
+				id=$scope.todolist[$scope.todolist.length-1].id+1;
+			}
+			$scope.todolist.push({id:id,name:$scope.newTask,isCompleted:false});
+			$scope.newTask='';
+			console.log(id);
+		}
 	}])
 
 //将angular传入	
